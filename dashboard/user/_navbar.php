df2aba1ff1e4a9ceb6081f1dad2d7eb836b0bfb4
HilfaRaisya496 <hilfaraisya01@gmail.com>
2025-12-20 16:58:49 +0700

    dashboard

--- dashboard/user/_navbar.php
+++ dashboard/user/_navbar.php
@@ -27,7 +27,7 @@ auth_required();
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-3">
         <li class="nav-item">
           <a class="nav-link <?= ($active ?? '') === 'dashboard' ? 'active' : '' ?>" href="user.php">
-            <i class="bi bi-grid-1x2 me-1"></i> Dashboardsssssss
+            <i class="bi bi-grid-1x2 me-1"></i> Dashboard
           </a>
         </li>
         <li class="nav-item">
